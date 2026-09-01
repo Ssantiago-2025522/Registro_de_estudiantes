4c6a4a24d4889a96bb3bf56be0f9f46e75f0464a
mariorobertoh29-cpu <mariorobertoh29@gmail.com>
2026-09-01 11:30:35 -0600

    se añadio la home

--- registro_estudiantes/src/app/app.routes.ts
+++ registro_estudiantes/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { FormularioEstudianteComponent } from './formulario-estudiante/formulario-estudiante';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'registro', component: FormularioEstudianteComponent },
+  { path: '**', redirectTo: '' } // Redirección por defecto si la ruta no existe
+];
